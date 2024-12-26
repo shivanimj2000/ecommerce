6e2885612832d7676a7399cab31bfa4a60983b88
shivanimj2000 <jadhavshivani3005@gmail.com>
2024-12-26 21:29:08 +0530

    First commit

---
+++ frontend/src/app/config/app-config.ts
@@ -0,0 +1,9 @@
+export default {
+  oidc: {
+    clientId: '0oalnslqmxYJWdVjw5d7',
+    issuer: 'https://dev-87627927.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    //redirectUri: 'https://luv2shop-ecommerce.netlify.app/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
